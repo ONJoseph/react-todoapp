d800e6f2e83ee06dcfe0722484fd08d2b3dadf33
ONJoseph <on.josephinfo@gmail.com>
2023-01-18 07:59:42 +0100

    add input field functionalities

---
+++ src/classBased/components/TodosList.js
@@ -0,0 +1,31 @@
+import React from "react"
+import TodoItem from "./TodoItem";
+
+class TodosList extends React.Component {
+  render() {
+    const completedStyle = {
+      fontStyle: "italic",
+      color: "#595959",
+      opacity: 0.4,
+      textDecoration: "line-through",
+    }
+    return (
+      <li className={styles.item}>
+        <input
+          type="checkbox"
+          className={styles.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+        </span>
+      </li>
+    )
+  }
+}
+
+export default TodosList
